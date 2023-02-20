e9c5d006c842200ad86470a1a797e432837031fb
Kkssrr <109816139+Kkssrr@users.noreply.github.com>
2023-02-20 19:53:48 +0530

    Update commands.py

--- CYNITE/commands.py
+++ CYNITE/commands.py
@@ -50,7 +50,7 @@ async def start(client, message):
                     InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about'),
                   ],[
-                    InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ ᴏᴜʀ ʏᴛ ᴄʜᴀɴɴᴇʟ', url="ks_official")
+                    InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ ᴏᴜʀ ʏᴛ ᴄʜᴀɴɴᴇʟ', url="https://youtube.com/@TechnicalCynite")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
